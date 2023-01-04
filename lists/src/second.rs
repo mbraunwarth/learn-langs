5972189d01265075fdd15d96928b5d4f212902cf
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-01-04 21:31:05 +0100

    implemented into_iter

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -39,6 +39,10 @@ impl<T> List<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        return IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -49,6 +53,16 @@ impl<T> Drop for List<T> {
         }
     }
 }
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -93,5 +107,17 @@ mod test {
 
         // test mutability
         list.peek_mut().map(|value| *value = 42);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
