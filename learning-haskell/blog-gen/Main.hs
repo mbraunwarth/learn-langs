5da67550edfe435f3c3f428a55bb09d1ee872e5a
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-03-03 17:14:47 +0100

    handling user IO

--- learning-haskell/blog-gen/Main.hs
+++ learning-haskell/blog-gen/Main.hs
@@ -1,15 +1,53 @@
 module Main where
 
-import Html
-import Convert
+import qualified Html
+import qualified Markup
+import Convert (convert)
+
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn $ render myhtml
+main = 
+  getArgs >>= 
+    \args ->
+      case args of
+        -- Reading from stdin and write to stdout
+        [] -> getContents >>= putStrLn . process "Empty Title"
 
-myhtml :: Html
-myhtml =
-  html_ 
-    "My Title" 
-    ( h1_ "Heading" <>
-      ( p_ "Paragraph #1" <> p_ "Paragraph #2" )
-    )
+        -- Reading from input file and write result to output file
+        [inFile, outFile] -> 
+          readFile inFile >>= \content ->
+            doesFileExist outFile >>= \exists ->
+              let 
+                writeResult = writeFile outFile (process inFile content)
+              in 
+                if exists
+                  then whenIO confirm writeResult
+                  else writeResult
+
+        -- Unallowed argument status
+        _ -> 
+          putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+
+
+-- | Parse a document to Markup, convert it to Html and render it to a string.
+process :: Html.Title -> String -> String
+process title = Html.render . convert title . Markup.parse
+
+confirm :: IO Bool
+confirm = 
+  putStrLn "Are you sure? (y/n)" *>
+    getLine >>= \answer ->
+      case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _   -> putStrLn "Invalid response. Use y or n" *>
+                 confirm
+
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action =
+  cond >>= \result ->
+    if result
+      then action
+      else pure ()
