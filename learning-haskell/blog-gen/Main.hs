222ae3f683068b2ddf2230182a681f83d6e42c29
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-03-04 02:07:14 +0100

    translated IO stuff to do notation

--- learning-haskell/blog-gen/Main.hs
+++ learning-haskell/blog-gen/Main.hs
@@ -8,46 +8,44 @@ import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
 main :: IO ()
-main = 
-  getArgs >>= 
-    \args ->
-      case args of
-        -- Reading from stdin and write to stdout
-        [] -> getContents >>= putStrLn . process "Empty Title"
+main = do
+  args <- getArgs
+  case args of 
+    -- Reading from stdin and write to stdout
+    [] -> do 
+      contents <- getContents
+      putStrLn $ process "Empty Title" contents
 
-        -- Reading from input file and write result to output file
-        [inFile, outFile] -> 
-          readFile inFile >>= \content ->
-            doesFileExist outFile >>= \exists ->
-              let 
-                writeResult = writeFile outFile (process inFile content)
-              in 
-                if exists
-                  then whenIO confirm writeResult
-                  else writeResult
+    -- Reading from input file and write result to output file
+    [inFile, outFile] -> do
+      content <- readFile inFile
+      exists <- doesFileExist outFile
+      let writeResult = writeFile outFile (process inFile content)
+      if exists 
+        then whenIO confirm writeResult 
+        else writeResult
 
-        -- Unallowed argument status
-        _ -> 
-          putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
-
+    -- Unallowed argument status
+    _ -> 
+      putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
 
 -- | Parse a document to Markup, convert it to Html and render it to a string.
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
 confirm :: IO Bool
-confirm = 
-  putStrLn "Are you sure? (y/n)" *>
-    getLine >>= \answer ->
-      case answer of
-        "y" -> pure True
-        "n" -> pure False
-        _   -> putStrLn "Invalid response. Use y or n" *>
-                 confirm
+confirm = do
+  putStrLn "Are you sure? (y/n)"
+  answer <- getLine
+  case answer of
+    "y" -> pure True
+    "n" -> pure False
+    _   -> do putStrLn "Please respond with 'y' or 'n'" 
+              confirm
 
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action =
-  cond >>= \result ->
-    if result
-      then action
-      else pure ()
+whenIO cond action = do
+  result <- cond
+  if result
+    then action
+    else pure ()
