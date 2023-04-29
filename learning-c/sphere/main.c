a07f1d0318dba6a815043f0f4aa926246c6a747a
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-04-29 17:19:08 +0200

    added todos

--- learning-c/sphere/main.c
+++ learning-c/sphere/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+/*
+ * TODO List
+ *  - Entry point?
+ *  - Application layer (lifecycle, events etc.)
+ *  - Window layer 
+ *    - Inputs, events
+ *  - Renderer (render graphics)
+ *  - Render API apstraction layer
+ *  - Debugging support
+ *  - Scripting
+ *  - Memory system
+ *  - Entity-component system (ECS)
+ *  - Physics
+ *  - File IO, VFS
+ *  - Build system
+ */
+
 int main() {
-  printf("Hello, World!\n");
+  const char *hello = "Hello, World!";
+  printf("%s\n", hello);
+
   return 0;
 }
