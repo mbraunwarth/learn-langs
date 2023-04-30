bd0b45a1f1cd87633ea11d9bbf8f1aac94e51836
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-05-01 00:43:09 +0200

    added not very elgant solution for problem 9

--- learning-ocaml/99-problems/p09.ml
+++ learning-ocaml/99-problems/p09.ml
@@ -1,1 +1,15 @@
 (* Pack consecutive duplicates of list elements into sublists *)
+let pack (xs: 'a list): ('a list list) = 
+  let rec pack' xs acc =
+    match xs with
+    | [] -> [[]]
+    | x :: y :: rest when x = y ->
+        pack' (y :: rest) (x :: acc)
+    | x :: y :: rest ->
+        (x :: acc) :: (pack' (y :: rest) [])
+    | x :: [] -> 
+        match acc with
+        | [] -> [xs]
+        | y :: _ when x = y -> [x :: acc]
+        | _ -> acc :: [[x]]
+  in pack' xs []
