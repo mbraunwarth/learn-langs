4c52d8cd56745e4f9d3f8c771f605cd388617129
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-05-12 17:41:17 +0200

    added string parsing

--- learning-haskell/write-a-scheme/Main.hs
+++ learning-haskell/write-a-scheme/Main.hs
@@ -3,11 +3,26 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottetList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Eq, Show)
+
 symbols :: Parser Char
 symbols = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  str <- many $ noneOf "\""
+  char '"'
+  return $ String str
 
 readExpr :: String -> String
 readExpr input = 
