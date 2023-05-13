bb52e56147ae33ce92568eea043474b2caaff926
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-05-13 16:26:14 +0200

    added parser for character literals

--- learning-haskell/write-a-scheme/Main.hs
+++ learning-haskell/write-a-scheme/Main.hs
@@ -9,8 +9,12 @@ data LispVal = Atom String
              | DottetList [LispVal] LispVal
              | Number Integer
              | String String
+             | Char Char
              | Bool Bool
              deriving (Eq, Show)
+
+delimiters :: Parser Char
+delimiters = oneOf "[]{}();\"'`|"
 
 symbols :: Parser Char
 symbols = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -31,12 +35,20 @@ parseAtom = do
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  str <- many $ noneOf "\""
+  str <- many $ oneOf "\n\t\r" <|> noneOf "\""
   char '"'
   return $ String str
 
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
+
+-- R5RS Characters: #\<character> or #\<character name>
+-- e.g. #\a #\B #\4 #\newline #\space etc.
+parseChar :: Parser LispVal
+parseChar = do 
+  string "#\\"
+  first <- char ' ' <|> symbols <|> digit <|> letter
+  return $ Char first
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
